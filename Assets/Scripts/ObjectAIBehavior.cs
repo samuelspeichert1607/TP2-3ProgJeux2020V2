92eab3cb7667fc12d3b76794f255fc95c6a34393
Alex Bouchard <alexbouchard04@gmail.com>
2020-11-10 15:13:53 -0500

    les ennemies ne peuvent pas te détecter à travers les murs (configuration pour rendre un mur invisible visuellement ou auditivement est possible)

---
+++ Assets/Scripts/ObjectAIBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAIBehavior : MonoBehaviour
+{
+    public bool canSoundPassThrough;
+    public bool canLightPassThrough;
+    public bool canActorsPassThrough;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerDetection.cs
+++ Assets/Scripts/PlayerDetection.cs
@@ -8,30 +8,55 @@ public class PlayerDetection : MonoBehaviour
 
     private DetectCollisions hearingCollider;
 
+    private Transform visionPoint;
+
+    private Transform centerOfMass;
+
     public GameObject detectedPlayer;
-
-    public float timeUntilGiveUp;//in seconds
-
-    public float TimeUntilChaseStops;//in seconds
 
     // Start is called before the first frame update
     void Start()
     {
         visionCollider = GetComponentInChildren<MeshCollider>().GetComponent<DetectCollisions>();
         hearingCollider = GetComponentInChildren<SphereCollider>().GetComponent<DetectCollisions>();
+        visionPoint = GetComponent<Transform>().Find("VisionPoint");
+        centerOfMass = GetComponent<Transform>().Find("CenterOfMass");
         detectedPlayer = null;
     }
 
     // Update is called once per frame
     void Update()
     {
+        detectedPlayer = null;
         List<Collider> visionColliders = visionCollider.getTriggers();
         for (int i = 0; i < visionColliders.Count; i++)
         {
             if (visionColliders[i] is CharacterController)
             {
-                detectedPlayer = visionColliders[i].gameObject;
-                timeUntilGiveUp = TimeUntilChaseStops;
+                GameObject player = visionColliders[i].gameObject;
+                Transform camera = player.GetComponent<Transform>().Find("StandardCamera");
+                Vector3 direction = camera.position - visionPoint.position;
+                RaycastHit[] hits;
+                hits = Physics.RaycastAll(visionPoint.position, direction, Mathf.Sqrt(Mathf.Pow(direction.x, 2) + Mathf.Pow(direction.z, 2)));
+                bool canSeeObject = true;
+                for (int j = 0; j < hits.Length; j++)
+                {
+                    if (hits[j].collider.GetComponent<ObjectAIBehavior>() != null)
+                    {
+                        ObjectAIBehavior behavior = hits[j].collider.GetComponent<ObjectAIBehavior>();
+                        if (!behavior.canLightPassThrough)
+                        {
+                            canSeeObject = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (canSeeObject)
+                {
+                    detectedPlayer = player;
+                }
+                break;
             }
         }
         List<Collider> hearingColliders = hearingCollider.getTriggers();
@@ -41,18 +66,32 @@ public class PlayerDetection : MonoBehaviour
             {
                 if (hearingColliders[i].GetComponentInParent<CharacterController>() != null)
                 {
-                    detectedPlayer = hearingColliders[i].GetComponentInParent<CharacterController>().gameObject;
-                    timeUntilGiveUp = TimeUntilChaseStops;
+                    GameObject player = hearingColliders[i].GetComponentInParent<CharacterController>().gameObject;
+                    Transform sound = player.GetComponent<Transform>().Find("SoundMade");
+                    Vector3 direction = sound.position - centerOfMass.position;
+                    RaycastHit[] hits;
+                    hits = Physics.RaycastAll(centerOfMass.position, direction, Mathf.Sqrt(Mathf.Pow(direction.x, 2) + Mathf.Pow(direction.z, 2)));
+                    bool canHearObject = true;
+                    for (int j = 0; j < hits.Length; j++)
+                    {
+                        if (hits[j].collider.GetComponent<ObjectAIBehavior>() != null)
+                        {
+                            ObjectAIBehavior behavior = hits[j].collider.GetComponent<ObjectAIBehavior>();
+                            if (!behavior.canSoundPassThrough)
+                            {
+                                canHearObject = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (canHearObject)
+                    {
+                        detectedPlayer = player;
+                    }
+                    break;
                 }
             }
-        }
-        if (detectedPlayer != null)
-        {
-            if (timeUntilGiveUp <= 0)
-            {
-                detectedPlayer = null;
-            }
-            timeUntilGiveUp -= Time.deltaTime;
         }
 
     }
