780303250656a89acc4998c5c5cc6ef1f3e6ac6d
samuelspeichert1607 <60676766+samuelspeichert1607@users.noreply.github.com>
2020-12-19 12:34:43 -0500

    Merging final de nos deux branches

---
+++ Assets/SynchVolume.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SynchVolume : MonoBehaviour
+{
+    private Slider music;
+    private Slider sfx;
+    private GlobalControl globalControl;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        globalControl = GameObject.Find("GlobalGameController").GetComponent<GlobalControl>();
+        music = GameObject.Find("Musique").GetComponent<Slider>();
+        sfx = GameObject.Find("SFX").GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        globalControl.MusicVolume = music.value;
+        globalControl.SoundEffectVolume = sfx.value;
+    }
+    
+}
